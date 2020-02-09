830f84a36b2ea26448d71bcda8402e1137258fa5
sagi levy <sagilevy@sagis-MacBook-Pro.local>
2020-02-09 20:41:46 +0200

    lint

--- src/main.py
+++ src/main.py
@@ -4,14 +4,15 @@ import os
 from athena import fetch_user_events
 
 def handler(event, context):
-
+    
+    client = boto3.client('athena')
+    
     athenaDatabase = os.environ['athenaDatabase']
     athenaTable = os.environ['athenaTable']
     outputBucket = os.environ['outputBucket']
     deletedUser = json.loads(json.dumps(event))['detail']['requestParameters']['userName']
 
-
-    client = boto3.client('athena')
     query_string = "SELECT userIdentity.userName, eventName, eventSource, requestParameters FROM \"{}\".\"{}\" WHERE userIdentity.userName = \'{}\' and (eventName like 'Create%' or eventName like 'Run%');".format(athenaDatabase, athenaTable, deletedUser)
     results = fetch_user_events(client, query_string, athenaDatabase ,outputBucket)
+    
     print (results)
